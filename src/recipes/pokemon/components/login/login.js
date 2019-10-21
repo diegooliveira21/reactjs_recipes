a5a15f2040c10622d2166402d35c5654a92a60da
Diego Oliveira <diego.oliveira@efabrika.com.br>
2019-10-21 16:38:17 -0300

    Added start of the login page

---
+++ src/recipes/pokemon/components/login/login.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./login.style.sass";
+
+import bubblesaur from "./bubblesaur";
+import squirtle from "./squirtle";
+import charmander from "./charmander";
+
+const Login = () => {
+  const pokemonList = [bubblesaur, squirtle, charmander];
+
+  const pokemonChoice = pokemonList.map(pokemon => {
+    <></>;
+  });
+};
